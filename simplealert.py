f347e783b38510136896ee5a41f603f719f247a3
krishna praveen gunnam <krishnapraveengunnam@krishnas-MacBook-Air.local>
2025-03-07 12:11:46 +0530

    second commit

--- simplealert.py
+++ simplealert.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
-
+print("hellow world")
 driver=webdriver.Firefox()
 driver.get("http://testautomationpractice.blogspot.com/")
 driver.maximize_window()
