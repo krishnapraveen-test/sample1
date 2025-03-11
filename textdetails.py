a91a289380f9cbb66e93fa64a704d0970ee09e5d
krishnapraveen-test <praveengunnam27@gmail.com>
2025-03-11 15:08:44 +0530

    Add files via upload

---
+++ textdetails.py
@@ -0,0 +1,14 @@
+
+name = "Krishna Praveen Gunnam"
+college = "VIT University"
+branch = "Computer Science and Engineering"
+year_of_passout = 2019
+percentage = 75
+
+
+print(" Personal & Academic Details ")
+print(f"Name: {name}")
+print(f"College: {college}")
+print(f"Branch: {branch}")
+print(f"Year of Pass Out: {year_of_passout}")
+print(f"Percentage: {percentage}%")
